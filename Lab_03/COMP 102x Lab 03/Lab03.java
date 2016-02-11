d7023e77cae905cc5549381801f93df601540ed3
ru66yx <chiedu@gmx.co.uk>
2016-02-11 23:32:51 +0000

    just latest changes

--- Lab_03/COMP 102x Lab 03/Lab03.java
+++ Lab_03/COMP 102x Lab 03/Lab03.java
@@ -8,9 +8,12 @@ public class Lab03
     {
      // Please write your code after this line
          
-    Canvas canvas = new Canvas();
+    Canvas canvas = new Canvas(400,400);
     ColorImage image = new ColorImage();
+    System.out.println(image.getWidth());
+    System.out.println(image.getHeight());
     canvas.add(image, 0, 0);
+    
         
         
         
@@ -19,12 +22,13 @@ public class Lab03
     public void loadTwoImagesOnTheSameCanvas()
     {
         // Please write your code after this line
-    Canvas canvas = new Canvas();
-    ColorImage image1 = new ColorImage();
-    canvas.add(image1, 0, 0);
+    Canvas canvas = new Canvas(800,400);
+    ColorImage image = new ColorImage();
+    canvas.add(image, 0, 0);
     ColorImage image2 = new ColorImage();
-    //Canvas canvas2 = new Canvas();
-    canvas.add(image2,450,450);
+    System.out.println(image2.getWidth());
+    System.out.println(image2.getHeight());
+    canvas.add(image2,400,0);
     
         
         
@@ -34,16 +38,26 @@ public class Lab03
     public void imageAddition() 
     {    
         // Please write your code after this line
-   
-        
-        
+   ColorImage image = new ColorImage();
+   ColorImage image2 = new ColorImage(); 
+   ColorImage image3 = image.add(image2); 
+   Canvas canvas = new Canvas(1220,400);
+   canvas.add(image, 0, 0);  
+   canvas.add(image2,410,0);
+   canvas.add(image3,820,0);
         
     }
    
     public void imageMultiplication() 
     {
         // Please write your code after this line
-        
+  ColorImage image = new ColorImage();
+  ColorImage image2 = new ColorImage(); 
+  ColorImage image3 = image.multiply(image2); 
+  Canvas canvas = new Canvas(1220,400);
+  canvas.add(image, 0, 0);  
+  canvas.add(image2,410,0);
+  canvas.add(image3,820,0);
         
         
         
