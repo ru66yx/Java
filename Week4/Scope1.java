d7682c9bec69adfa3d8c663da185e1c1c9cd2bfe
Uche <chiedu@gmx.co.uk>
2016-02-15 19:31:02 +0000

    2nd commit

---
+++ Week4/Scope1.java
@@ -0,0 +1,45 @@
+
+/**
+ * Write a description of class Scope1 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import comp102x.IO;
+
+
+public class Scope1
+{
+    
+
+    private int c =1;
+    private int a = 2;
+    
+    public int one (int a, int b) {
+    
+        c = a * 2;
+        a = 3 + b;
+        b = b + 1;
+
+        IO.outputln("One: " + a + "," + b + "," + c);
+        return b;
+    }
+
+    public int two (int a, int b) {
+    
+        c = one (b, a);
+        this.a = a + 1;
+        IO.outputln("Two: " + a + "," + b + "," + c);
+        return a;
+   }
+   
+    public static void main(String[ ] args) {
+    
+        Scope1 s = new Scope1();
+        int a = 4; 
+        int b = 5;
+
+        b = s.two (a, b);
+        IO.outputln("Main: " + s.a + "," + b + "," + s.c);
+   }
+}
