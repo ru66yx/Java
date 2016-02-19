e8681884f5b4111d551f29e7b78b66106502e3eb
Uche <chiedu@gmx.co.uk>
2016-02-19 16:25:28 +0000

    some minor changes

--- COMP 102x Lab 01/Lab01.java
+++ COMP 102x Lab 01/Lab01.java
@@ -14,27 +14,13 @@ int result = i - (int)f;
 
 System.out.println("Hello, world!");
 System.out.println("Hello, everyone!");
-System.out.println( 0.1 * 0.1);
-
-System.out.println(i);
-System.out.println(result);
-System.out.println(f);
-System.out.println(0.1 * 0.1 == 0.01);
-
-i = 8;
-int j = 4;
-i -= 2;
-j *= i;
-result = i - j;
-System.out.println(result);  //-18
-
-int[] array = {2, 1, 0, 2, 3, 4};
-
-System.out.print(array[3]);
-System.out.print(array[4]);
-System.out.print(array[5]);
 
 
+
+int[] a = {4, 3, 2, 1};
+for (i = 0; i < 4; i++)
+System.out.println(a[i]);
+    
 
 
  
